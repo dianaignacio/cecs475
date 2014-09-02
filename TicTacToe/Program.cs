4ccaaffb289140af07b681651ffdca6efb7b07ce
Diana Ignacio <diana.ignacio@student.csulb.edu>
2014-09-02 13:27:26 -0700

    Included input validation for player moves

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,40 +9,61 @@ namespace TicTacToe
     public class TicTacToe
     {
         private const int BOARDSIZE = 3; // size of the board
-        private int[,] board = new int[,] { { 0, 0 }, { 0, 1 }, { 0, 2 }, 
-                                            { 1, 0 }, { 1, 1 }, { 1, 2 } ,
-                                            { 2, 0 }, { 2, 1 }, { 2, 2 } };// board representation
+        private int[,] board = new int[,] { { }, { }, { }, 
+                                            { }, { }, { },
+                                            { }, { }, { } };// board representation
+
+        public int this[int row, int column]
+        {
+            get { return board[row, column]; }
+            set { board[row, column] = value; }
+        }
+
         internal void PrintBoard()
         {
-            for (int i = 0; i < 12; i++)
+            for (int y = 0; y < board.Length; ++y)
             {
-                if (i % 4 == 0) 
+                for (int x = 0; x < board.Length; ++x)
                 {
-                    Console.WriteLine(" ----------------------- ");
+                    Console.Write(board[y, x]);
+                    if (x < board.Length-1)
+                    {
+                        Console.Write('|');
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("|       |       |       |");}
-                }
-            Console.WriteLine(" ----------------------- ");
+                Console.Write('\n');
+            }
         }
 
         void GetPlayer1Move()
         {
             Console.WriteLine("Player 1's turn");
-            Console.Write("Player 1: Enter row ( 0 <= row < 3: ");
-            string rowVal = Console.ReadLine();
-            Console.Write("Player 1: Enter column ( 0 <= row < 3: ");
-            string columnVal = Console.ReadLine();
+            int rowVal = 0;
+            int columnVal = 0;
+            rowVal = MoveCheck("Player 1: Enter row ( 0 <= row < 3: ", 0, 3);
+            columnVal = MoveCheck("Player 1: Enter column ( 0 <= column < 3: ", 0, 3);
         }
 
         void GetPlayer2Move()
         {
             Console.WriteLine("Player 2's turn");
-            Console.Write("Player 2: Enter row ( 0 <= row < 3: ");
-            string rowVal = Console.ReadLine();
-            Console.Write("Player 2: Enter column ( 0 <= row < 3: ");
-            string columnVal = Console.ReadLine();
+            int rowVal = 0;
+            int columnVal = 0;
+            rowVal = MoveCheck("Player 2: Enter row ( 0 <= row < 3: ", 0, 3);
+            columnVal = MoveCheck("Player 2: Enter column ( 0 <= column < 3: ", 0, 3);
+        }
+
+        int MoveCheck(string message, int min, int max)
+        {
+            Console.Write(message);
+            string line = Console.ReadLine();
+            int number;
+            while (!(int.TryParse(line, out number) && min <= number && number < max))
+            {
+                Console.Write("\nInvalid entry. Please retry : ");
+                line = Console.ReadLine();
+            }
+            return number;
         }
 
         internal void Play()
