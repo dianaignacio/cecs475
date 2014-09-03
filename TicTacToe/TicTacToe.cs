8f69261a7fa9bf292fa4a24b7d994b3b9b196c87
Diana Ignacio <diana.ignacio@student.csulb.edu>
2014-09-03 14:11:20 -0700

    Takes user input and displays on grid for both players

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -20,8 +20,7 @@ public class TicTacToe
                 }
                   Console.WriteLine("|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _|");
         }
-        
-        
+                
         void GetPlayer1Move()
         {
             Console.WriteLine("Player 1's turn");
@@ -29,6 +28,8 @@ public class TicTacToe
             int columnVal = 0;
             rowVal = MoveCheck("Player 1: Enter row ( 0 <= row < 3: ", 0, 3);
             columnVal = MoveCheck("Player 1: Enter column ( 0 <= column < 3: ", 0, 3);
+            board[rowVal, columnVal] = 1;
+            PlayMove(board);
         }
 
         void GetPlayer2Move()
@@ -38,8 +39,10 @@ public class TicTacToe
             int columnVal = 0;
             rowVal = MoveCheck("Player 2: Enter row ( 0 <= row < 3: ", 0, 3);
             columnVal = MoveCheck("Player 2: Enter column ( 0 <= column < 3: ", 0, 3);
+            board[rowVal, columnVal] = 2;
+            PlayMove(board);
         }
-
+        
         int MoveCheck(string message, int min, int max)
         {
             Console.Write(message);
@@ -53,12 +56,26 @@ public class TicTacToe
             return number;
         }
 
-        void PlayMove(int[,] arr, int playerIndex)
+        void PlayMove(int[,] arr)
         {
-            PrintBoard();
-            Console.WriteLine("Player {0}'s turn!", playerIndex + 1);
-            //var move = playerMove();
-            //board[move.Item1, move.Item2] = players[playerIndex];
+            for (int x = 0; x < BOARDSIZE; x++)
+            {
+                Console.WriteLine(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
+                Console.WriteLine("|         |         |         |");
+                for (int y = 0; y < BOARDSIZE; y++)
+                {
+                    if (board[x, y] == 0)
+                    {
+                        Console.Write("|         ", board[x, y]);
+                    }
+                    else
+                    {
+                        Console.Write("|    {0}    ", board[x, y]);
+                    }
+                }
+                Console.WriteLine("|");
+            }  // End for-loop
+            Console.WriteLine("|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _|");
         }
         
         public void Play()
@@ -67,7 +84,6 @@ public class TicTacToe
             while (!endOfGame)
             {
                 GetPlayer1Move();
-                PrintBoard();
                 GetPlayer2Move();
             }
             PrintBoard();
