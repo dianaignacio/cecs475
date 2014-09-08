5fe5215b24eee96137fe4f23ab5819dc013a2e40
Diana Ignacio <diana.ignacio@student.csulb.edu>
2014-09-08 01:17:38 -0700

    Included appropriate changes to inheritance

---
+++ Lab2/Lab2Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTest
+{
+    class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            IPayable[] payableObjects = new IPayable[8];
+            payableObjects[0] = new SalariedEmployee("John", "Smith", "111-11-1111", 700M);
+            payableObjects[1] = new SalariedEmployee("Antonio", "Smith", "555-55-5555", 800M);
+            payableObjects[2] = new SalariedEmployee("Victor", "Smith", "444-44-4444", 600M);
+            payableObjects[3] = new HourlyEmployee("Karen", "Price", "222-22-2222", 16.75M, 40M);
+            payableObjects[4] = new HourlyEmployee("Ruben", "Zamora", "666-66-6666", 20.00M, 40M);
+            payableObjects[5] = new CommissionEmployee("Sue", "Jones", "333-33-3333", 10000M, .06M);
+            payableObjects[6] = new BasePlusCommissionEmployee("Bob", "Lewis", "777-77-7777", 5000M, .04M, 300M);
+            payableObjects[7] = new BasePlusCommissionEmployee("Lee", "Duarte", "888-88-888", 5000M, .04M, 300M);
+        }
+    }
+}
